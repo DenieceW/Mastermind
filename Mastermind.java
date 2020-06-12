7b5ddff50e512117310c9a9cba8037aadc11fb67
Deniece <52138289+DenieceW@users.noreply.github.com>
2020-06-12 17:28:16 +0200

    Update Mastermind.java

--- Mastermind.java
+++ Mastermind.java
@@ -10,23 +10,21 @@ public class Mastermind {
     void start(){
         printWelkom();
         computer.genereerComputerCode();
+        System.out.println(computer.getVerborgenCode());
         while (true) {
-            System.out.println("Vul jouw code in. Je kunt kiezen uit a,b,c,d,e,f.(houd er rekening mee dat 1 letter vaker kan voorkomen). Wil je stoppen? type dan 'q': ");
-            String spelersInput = scanner.nextLine().toLowerCase().
-                    replaceAll("[ghijklmnoprstuwvxyz]","").replaceAll("\\d+","");
+            meldingInvoerCode();
+            String spelersInput = scanner.nextLine().toLowerCase().replaceAll("[ghijklmnoprstuwvxyz]","").
+                    replaceAll("\\d+","");
             //replaceAll etc. negeert alle nummers/letters na de G (behalve Q) in de string en vervangt dit met "".
-
             if(spelersInput.equals("q")){
-                System.out.println("De verborgen code was " + computer.getVerborgenCode());
+                meldingStopSpel();
                 break;
             }else if (spelersInput.isEmpty()){
-                System.out.println("Het veld is leeg, je hebt nummers ingevoerd " +
-                        "en/of letters gekozen die niet mee tellen! Doe nogmaals een poging!");
+                meldingOngeldigeInvoer();
             }else if(spelersInput.length() != computer.getVerborgenCode().length()) {
-                System.out.println("Je moet vier letters kiezen uit a,b,c,d,e of f!");
+                meldingOnvolledigeInvoer(spelersInput);
             }else if(spelersInput.equals(computer.getVerborgenCode())){
-                System.out.println("YES! Je hebt de code gekraakt in " + computer.getAantalKeerGeraden()
-                        + " pogingen! De code was " + computer.getVerborgenCode());
+               meldingGewonnen();
                 break;
             }else computer.checkAntwoordSpeler(spelersInput);
         }
@@ -36,5 +34,28 @@ public class Mastermind {
         System.out.println("Welkom bij Mastermind! Je moet een code, " +
                 "bestaande uit vier letters kraken! Gaat dit je lukken?");
         System.out.println("************************************************************************************************");
+    }
+
+    void meldingInvoerCode(){
+        System.out.println("Vul jouw code in. Je kunt kiezen uit a,b,c,d,e,f " +
+                "(houd er rekening mee dat 1 letter vaker kan voorkomen). Wil je stoppen? type dan 'q': ");
+    }
+
+    void meldingStopSpel(){
+        System.out.println("De verborgen code was " + computer.getVerborgenCode());
+    }
+
+    void meldingOngeldigeInvoer(){
+        System.out.println("Het veld is leeg, je hebt nummers ingevoerd " +
+                "en/of letters gekozen die niet mee tellen! Doe nogmaals een poging!");
+    }
+    void meldingOnvolledigeInvoer(String invoer){
+        System.out.println("Je hebt " + (invoer.length()) +  " " +
+                "karakters ingevoerd. Je mag maximaal vier letters invoeren (a,b,c,d,e of f).");
+    }
+    void meldingGewonnen(){
+        System.out.println("YES! Je hebt de code gekraakt in " + computer.getAantalKeerGeraden()
+                + " pogingen! De code was " + computer.getVerborgenCode());
+
     }
 }
