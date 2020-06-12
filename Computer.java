87ed74475a59b3f86911a4106a95a2af47445378
Deniece <52138289+DenieceW@users.noreply.github.com>
2020-06-12 16:29:18 +0200

    Update Computer.java
    methode toegevoegd om dubbele letters in gegenereerde pc code te checken. Dubbele letters werken soms wel/niet.

--- Computer.java
+++ Computer.java
@@ -6,6 +6,7 @@ public class Computer {
     String computerCode = "";
     int aantalCorrecteLettersOpJuistePlek = 0;
     int aantalCorrecteLettersOpOnjuistePlek = 0;
+    int aantalDubbeleCorrecteLettersOpOnjuistePlek = 0;
     int maxAantalLetters = 4;
     int aantalKeerGeraden = 1;
 
@@ -23,25 +24,60 @@ public class Computer {
     }
 
     void checkAntwoordSpeler(String spelersCode) {
-        for (int i = 0; i < computerCode.length(); i++) {
-            if (spelersCode.charAt(i) == computerCode.charAt(i)) { //checken voor dezelfde  plek en letter.
-                aantalCorrecteLettersOpJuistePlek++;
-            } else if (spelersCode.charAt(i) != computerCode.charAt(i) && //check voor juiste letters die op verkeerde plek staan
-                    spelersCode.contains(String.valueOf(computerCode.charAt(i)))){
-                aantalCorrecteLettersOpOnjuistePlek++;
+        //werkt soms voor dubbele letters, soms ook niet, but hey its something.
+        if(checkVoorDubbeleLetters()) {
+            System.out.println("pc code heeft dubbele letters");
+            for (int i = 0; i < computerCode.length(); i++) {
+                if ((computerCode.charAt(i) == spelersCode.charAt(i))) { //checken voor dezelfde  plek en letter.
+                    aantalCorrecteLettersOpJuistePlek++;
+                }
+                if (spelersCode.contains(String.valueOf(computerCode.charAt(i))) &&
+                        spelersCode.charAt(i) != computerCode.charAt(i)) {
+                    aantalCorrecteLettersOpOnjuistePlek++;
+                    System.out.println("check 1");
+                }else if (spelersCode.substring(i).equals(computerCode.substring(i))) { //als speler toevallig de dezelfde dubbele volgorde invoert als pccode met dubbele letters.
+                    aantalCorrecteLettersOpOnjuistePlek--;
+                    System.out.println("check 2");
+                 }
             }
         }
+        //hieronder werkt voor vier en twee verschillende letters in de code
+        else
+            for (int i = 0; i < computerCode.length(); i++) {
+                if ((computerCode.charAt(i) == spelersCode.charAt(i))) { //checken voor dezelfde  plek en letter.
+                    aantalCorrecteLettersOpJuistePlek++;
+                    System.out.println("pc code heeft geen dubbele letters");
+                } else if (spelersCode.contains(String.valueOf(computerCode.charAt(i))) && //als de code deze juist letter bevat, maar niet op de correcte plek.
+                        spelersCode.charAt(i) != computerCode.charAt(i)) {
+                    aantalCorrecteLettersOpOnjuistePlek++;
+                }
+            }
 
         computerReactie(aantalCorrecteLettersOpJuistePlek,aantalCorrecteLettersOpOnjuistePlek);
-
         //resetten naar 0 zodat de vorige waardes niet worden meegenomen
         aantalCorrecteLettersOpJuistePlek = 0;
         aantalCorrecteLettersOpOnjuistePlek = 0;
+        aantalDubbeleCorrecteLettersOpOnjuistePlek = 0;
         aantalKeerGeraden++; //elke keer wanneer de methode wordt aangeroepen in class mastermind, neemt het aantal geraden pogingen met 1 toe.
     }
 
     int getAantalKeerGeraden(){
         return aantalKeerGeraden;
+    }
+
+    private boolean checkVoorDubbeleLetters(){
+        char[] pcCode = computerCode.toCharArray();
+        //hier checken of de computer dubbele letters heeft.
+        for (var i = 0; i < pcCode.length; i++) {
+            for (var j = 0; j < pcCode.length; j++) {
+                if (i != j) {
+                    if (pcCode[i] == pcCode[j]) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 
     void computerReactie(int juisteplek, int onjuisteplek){
@@ -57,11 +93,15 @@ public class Computer {
         }
         if (onjuisteplek < enkeleLetter){ //zodat de min getallen door de dubbele letter methode niet worden meegenomen.
             System.out.println("Je hebt 0 correcte letters op de onjuiste plek staan");
+
         } else if (onjuisteplek > enkeleLetter) {
             System.out.println("Je hebt " + onjuisteplek + " correcte letters op een onjuiste plek staan");
         }
     }
 }
+
+
+
 
 
 
